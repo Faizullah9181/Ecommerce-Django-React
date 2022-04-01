cf2c5c3440b08b2ed468e99b7d82f5793b7e7fcd
FAIZULLAH <faizullah9181@gmail.com>
2022-04-02 01:11:02 +0530

    first commit

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import Rating  from './Rating'
+import { Link } from 'react-router-dom'
+
+function Product({ product }) {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} />
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`} style={{ textDecoration: "none" }}>
+          <Card.Title as="div">
+            <strong> {product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as="div">
+          <div className="my-3">
+            
+            <Rating value={product.rating} text={`${product.numReviews} reviews` } color={'#e0d870'}/>
+          </div>
+        </Card.Text>
+
+        <Card.Text as="h3" style={{ color: "#696969", fontWeight: "bold" }}>
+          ${product.price}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Product;
