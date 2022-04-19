0e051d5fe3a518ffc4c482d34871bcd076f79527
FAIZULLAH <faizullah9181@gmail.com>
2022-04-19 12:16:18 +0530

    .

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'senxiu&i$l-a(4yka_b0j#)upa37^*g5q#r9x2@fgz!yzvlfv&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alphmart.herokuapp.com','127.0.0.1', 'localhost']
 
 
 # Application definition
